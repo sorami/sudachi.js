653c455329d8b62f7da786d5e889faaa6780328a
Sorami Hisamoto <s@89.io>
2021-02-23 11:22:18 +0900

    Implement

--- src/dictionary/wordParameterList.ts
+++ src/dictionary/wordParameterList.ts
@@ -1,9 +1,9 @@
 export class WordParameterList {
     private ELEMENT_SIZE = 2 * 3
 
-    private buf: Buffer
+    private readonly buf: Buffer
     readonly size: number
-    private offset: number
+    private readonly offset: number
 
     constructor(buf: Buffer, offset: number) {
         this.buf = buf
@@ -13,5 +13,21 @@ export class WordParameterList {
 
     storageSize(): number {
         return 4 + this.ELEMENT_SIZE * this.size
+    }
+
+    getLeftId(wordId: number): number {
+        return this.buf.readInt16LE(this.offset + this.ELEMENT_SIZE * wordId)
+    }
+
+    getRightId(wordId: number): number {
+        return this.buf.readInt16LE(
+            this.offset + this.ELEMENT_SIZE * wordId + 2
+        )
+    }
+
+    getCost(wordId: number): number {
+        return this.buf.readInt16LE(
+            this.offset + this.ELEMENT_SIZE * wordId + 4
+        )
     }
 }
